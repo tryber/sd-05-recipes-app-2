a032eed727b69d349fa120ff02abaf2450eefbb9
marinabrcls <barcelos.marina@gmail.com>
2020-09-09 17:03:38 -0300

    context-dev-tools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { debugContextDevtool } from 'react-context-devtool';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Provider from './contexts/Provider';
 
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
+const container = document.getElementById('root');
+
+ReactDOM.render(<Provider><App /></Provider>, container);
+
+debugContextDevtool(container);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
