3d3467c8f5886f1fd13b2e6fda766768ed5a3b52
marinabrcls <barcelos.marina@gmail.com>
2020-09-10 20:55:51 -0300

    testes 1-4 de um total de 7

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -2,20 +2,27 @@ import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import Login from '../pages/Login/Login';
 import renderWithRouter from '../services/renderWithRouter';
+import Provider from '../contexts/Provider';
 
 describe('Todos os elementos devem respeitar os atributos descritos no protótipo para a tela de login', () => {
   it('Tem os data-testids email-input, password-input e login-submit-btn', () => {
-    const { container } = renderWithRouter(<Login />);
-    expect(container.getByTestId("email-input")).toBeInTheDocument();
-    expect(container.getByTestId("password-input")).toBeInTheDocument();
-    expect(container.getByTestId("login-submit-btn")).toBeInTheDocument();
+    const { getByTestId } = renderWithRouter(
+      <Provider>
+        <Login />
+      </Provider>);
+    expect(getByTestId("email-input")).toBeInTheDocument();
+    expect(getByTestId("password-input")).toBeInTheDocument();
+    expect(getByTestId("login-submit-btn")).toBeInTheDocument();
   });
 });
 
 describe('A pessoa deve conseguir escrever seu email no input de email', () => {
   it('É possível escrever o email', () => {
-    const { container } = renderWithRouter(<Login />);
-    const emailInput = container.getElementByLabelText(/E-mail:/i);
+    const { getByLabelText } = renderWithRouter(
+      <Provider>
+        <Login />
+      </Provider>);
+    const emailInput = getByLabelText(/E-mail:/i);
     fireEvent.change(emailInput, { target: { value: 'myemail@emailprovider.com' } });
     expect(emailInput.value).toBe('myemail@emailprovider.com');
   });
@@ -23,51 +30,70 @@ describe('A pessoa deve conseguir escrever seu email no input de email', () => {
 
 describe('A pessoa deve conseguir escrever sua senha no input de senha', () => {
   it('É possível escrever a senha', () => {
-    const { container } = renderWithRouter(<Login />);
-    const passwordInput = container.getElementByLabelText(/Senha:/i);
+    const { getByLabelText } = renderWithRouter(
+      <Provider>
+        <Login />
+      </Provider>);
+    const passwordInput = getByLabelText(/Senha:/i);
     fireEvent.change(passwordInput, { target: { value: '12345678' } });
     expect(passwordInput.value).toBe('12345678');
   });
 });
 
-/* describe('O formulário só fica válido após um email válido e uma senha de mais de 6 caracteres serem preenchidos', () => {
+describe('O formulário só fica válido após um email válido e uma senha de mais de 6 caracteres serem preenchidos', () => {
   it('O botão deve estar desativado se o email for inválido', () => {
-    const { container } = renderWithRouter(<Login />);
+    const { getByRole, getByLabelText } = renderWithRouter(
+      <Provider>
+        <Login />
+      </Provider>);
+    const submitBtn = getByRole('button', { name: /Entrar/i });
+    const emailInput = getByLabelText(/E-mail:/i);
+    const passwordInput = getByLabelText(/Senha:/i);
 
-    cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+    expect(submitBtn).toBeDisabled();
 
-    cy.get('[data-testid="email-input"]').type('email@mail');
-    cy.get('[data-testid="password-input"]').type('1234567');
+    fireEvent.change(emailInput, { target: { value: 'email@email' } });
+    fireEvent.change(passwordInput, { target: { value: '1234567' } });
+    expect(submitBtn).toBeDisabled();
 
-    cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
-
-    cy.get('[data-testid="email-input"]').clear().type('email.com');
-
-    cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+    fireEvent.change(emailInput, { target: { value: 'email.com' } });
+    expect(submitBtn).toBeDisabled();
   });
 
   it('O botão deve estar desativado se a senha deve tiver 6 caracteres ou menos', () => {
-    const { container } = renderWithRouter(<Login />);
+    const { getByRole, getByLabelText } = renderWithRouter(
+      <Provider>
+        <Login />
+      </Provider>);
+    const submitBtn = getByRole('button', { name: /Entrar/i });
+    const emailInput = getByLabelText(/E-mail:/i);
+    const passwordInput = getByLabelText(/Senha:/i);
 
-    cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+    expect(submitBtn).toBeDisabled();
 
-    cy.get('[data-testid="email-input"]').type('email@mail.com');
-    cy.get('[data-testid="password-input"]').type('123456');
+    fireEvent.change(emailInput, { target: { value: 'myemail@emailprovider.com' } });
+    fireEvent.change(passwordInput, { target: { value: '1234' } });
 
-    cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+    expect(submitBtn).toBeDisabled();
   });
 
   it('O botão deve estar ativado se o email e a senha forem válidos', () => {
-    const { container } = renderWithRouter(<Login />);
+    const { getByRole, getByLabelText } = renderWithRouter(
+      <Provider>
+        <Login />
+      </Provider>);
+    const submitBtn = getByRole('button', { name: /Entrar/i });
+    const emailInput = getByLabelText(/E-mail:/i);
+    const passwordInput = getByLabelText(/Senha:/i);
 
-    cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+    expect(submitBtn).toBeDisabled();
 
-    cy.get('[data-testid="email-input"]').type('email@mail.com');
-    cy.get('[data-testid="password-input"]').type('1234567');
+    fireEvent.change(emailInput, { target: { value: 'myemail@emailprovider.com' } });
+    fireEvent.change(passwordInput, { target: { value: '12345678' } });
 
-    cy.get('[data-testid="login-submit-btn"]').should('not.be.disabled');
+    expect(submitBtn).toBeEnabled();
   });
-}); */
+});
 
 /* describe('Após a submissão, 2 tokens devem ser salvos em localStorage identificados pelas chaves mealsToken e cocktailsToken', () => {
   it('Após a submissão mealsToken e cocktailsToken devem estar salvos em localStorage', () => {
