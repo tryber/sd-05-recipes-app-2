a66a5766426b4e55d8353551577cd7770f27263d
rfreitasbatista <rfreitasbatista@gmail.com>
2020-09-10 21:48:12 -0300

    Atualização

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import Card from '../../components/Card';
 import * as api from '../../services/api';
 import Header from '../../components/Header';
 import SearchBar from '../../components/SearchBar';
 import Footer from '../../components/Footer';
+import AppContext from '../../contexts/AppContext';
 
 function Home() {
   const history = useHistory();
+  const { selecCategory, filteredData } = useContext(AppContext);
   const [defaultCards, setDefaultCards] = useState([]);
   const [mealBool, setMealBool] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
     if (history.location.pathname === '/comidas') {
       setMealBool(true);
+      console.log(selecCategory);
       api.defaultMeals().then((data) => {
         setDefaultCards(data.meals);
         setLoading(false);
