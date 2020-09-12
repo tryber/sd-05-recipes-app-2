6ea8aa89f5d4ac088cedb0fba9e6737f52fa2e2c
marinabrcls <barcelos.marina@gmail.com>
2020-09-12 01:40:31 -0300

    estamos no caminho! por hoje é só

--- src/pages/Comidas/ComidaDetalhes/ComidaDetalhes.js
+++ src/pages/Comidas/ComidaDetalhes/ComidaDetalhes.js
@@ -4,6 +4,32 @@ import AppContext from '../../../contexts/AppContext';
 import shareIcon from '../../../images/shareIcon.svg';
 import whiteHeartIcon from '../../../images/whiteHeartIcon.svg';
 
+function ingredientsList(details) {
+  const quantities = [];
+  const ingredients = [];
+
+  Object.entries(details).forEach((element) => {
+    if (element[0].includes('strMeasure')) {
+      quantities.push(element[1]);
+    }
+    if (element[0].includes('strIngredient')) {
+      ingredients.push(element[1]);
+    }
+  });
+
+  return (
+    <div>
+      <h3>Ingredients</h3>
+      <ul>
+        {quantities.map((element, index) => (
+          <li data-testid={`${index}-ingredient-name-and-measure`}>
+            {ingredients[index]} - {element}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 function ComidaDetalhes() {
   const { selectedId, loading, setLoading, details, setDetails } = useContext(AppContext);
@@ -11,9 +37,7 @@ function ComidaDetalhes() {
   useEffect(() => {
     setLoading(true);
     api.byMealId(selectedId).then((data) => {
-      console.log(data);
-      console.log(selectedId);
-      // setDetails(data.meals[0]);
+      setDetails(data.meals[0]);
       setLoading(false);
     })
   }, [])
@@ -22,49 +46,31 @@ function ComidaDetalhes() {
   return (
     <div>
       <div className="details-header">
-        {/*         <img
-          alt={recipe_name}
+        <img
+          alt={details.strMeal}
           data-testid="recipe-photo"
-          src={recipe_thumb}
-        /> */}
-        <h2 data-testid="recipe-title">Receita-Título</h2>
-        <h4 data-testid="recipe-category">Receita-Categoria</h4>
+          src={details.strMealThumb}
+        />
+        <h2 data-testid="recipe-title">{details.strMeal}</h2>
+        <h4 data-testid="recipe-category">{details.strCategory}</h4>
         <img alt="share button" data-testid="share-btn" src={shareIcon} />
         <img alt="favorite button" data-testid="favorite-btn" src={whiteHeartIcon} />
       </div>
       <div className="details-body">
         <h3>Ingredients</h3>
-        {/*         <li data-testid={`${index}-ingredient-name-and-measure`}>Um certo ingrediente</li> */}
+        {ingredientsList(details)}
         <h3>Instructions</h3>
-        <p data-testid="instructions">Descrição das instruções aqui</p>
+        <p data-testid="instructions">{details.strInstructions}</p>
         <h3>Vídeo</h3>
-        {/*         <video width="320" height="240" controls>
-          <source data-testid="video" src={} type="video/mp4" />
-        </video> */}
+        <video width="320" height="240" controls>
+          <source data-testid="video" src={details.strYoutube} type="video/mp4" />
+        </video>
         <h3>Recomendadas</h3>
-        <p>Aqui vai ter um carrossel</p>
+        <p>Carossel de recomendações</p>
+        <button data-testid="start-recipe-btn"> Iniciar receita</button>
       </div>
-      <button
-        type="button"
-        data-testid="start-recipe-btn"
-      >
-        Iniciar receita
-      </button>
     </div>
   );
 }
 
 export default ComidaDetalhes;
-
-// todos os data-testid
-// data-testid="recipe-photo" OK
-// data-testid="recipe-title" OK
-// data-testid="share-btn" OK
-// data-testid="favorite-btn" OK
-// data-testid="recipe-category" OK
-// data-testid=`${index}-ingredient-name-and-measure` OK
-// data-testid="instructions" OK
-// data-testid="video" OK
-// data-testid=`${index}-recomendation-card` - não tem
-// data-testid=`${index}-recomendation-title` - não tem
-// data-testid="start-recipe-btn" OK
