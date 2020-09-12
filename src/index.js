1f660ca6f2174bd4e1ba551dda1543ea9733d1c8
rfreitasbatista <rfreitasbatista@gmail.com>
2020-09-12 16:03:49 -0300

    CC 2 issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { debugContextDevtool } from 'react-context-devtool';
+/* import { debugContextDevtool } from 'react-context-devtool'; */
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,8 +8,8 @@ import * as serviceWorker from './serviceWorker';
 const container = document.getElementById('root');
 
 ReactDOM.render(<App />, container);
-/* 
-debugContextDevtool(container); */
+
+/* debugContextDevtool(container); */
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
