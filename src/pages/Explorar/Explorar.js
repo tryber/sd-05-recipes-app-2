4fe3cec1b47386fbbf8937348f3a65dfc23b638d
RenataNovais <rnovaiscs@gmail.com>
2020-09-13 17:28:21 -0300

    criando explorar para aplicar css

--- src/pages/Explorar/Explorar.js
+++ src/pages/Explorar/Explorar.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import Header from '../../components/Header';
+import { useHistory } from 'react-router-dom';
 
 export default function Explorar() {
+  const history = useHistory();
   return (
-    <Header />
+    <div>
+      <Header />
+      <div>
+        <button onClick={() => history.push('/explorar/comidas')}>
+          Explorar Comidas
+        </button>
+        <button onClick={() => history.push('/explorar/bebidas')}>
+          Explorar Comidas
+        </button>
+      </div>
+    </div>
   );
 }
