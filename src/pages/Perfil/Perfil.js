51fd9eee5cffe7dce6b6b34fa3dad67d1fa1443a
RenataNovais <rnovaiscs@gmail.com>
2020-09-13 19:07:42 -0300

    criados botões de redirecionamento. Botão sair limpa localstorage

--- src/pages/Perfil/Perfil.js
+++ src/pages/Perfil/Perfil.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import AppContext from '../../contexts/AppContext';
 
 export default function Perfil() {
+  const history = useHistory();
+  const { email } = useContext(AppContext)
+
+  const handleClick = () => {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
-    <Header />
+    <div>
+      <Header />
+      <div>
+        {email}
+      </div>
+      <div>
+        <button className="btn" onClick={() => history.push('/receitas-feitas')}>
+          Receitas Feitas
+        </button>
+        <button className="btn" onClick={() => history.push('/receitas-favoritas')}>
+          Receitas Favoritas
+        </button>
+        <button className="btn" onClick={() => handleClick()}>
+          Sair
+        </button>
+      </div>
+      <Footer />
+    </div>
   );
 }
