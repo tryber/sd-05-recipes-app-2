ceb71da7db74d2813df24a14f2813ff1175406ff
RenataNovais <rnovaiscs@gmail.com>
2020-09-13 17:53:47 -0300

    css basico pagina explorar bebidas e comidas

--- src/pages/Explorar/Explorar.js
+++ src/pages/Explorar/Explorar.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import Header from '../../components/Header';
 import { useHistory } from 'react-router-dom';
+import './explorar.css';
+import Header from '../../components/Header';
 
 export default function Explorar() {
   const history = useHistory();
   return (
     <div>
       <Header />
-      <div>
-        <button onClick={() => history.push('/explorar/comidas')}>
+      <div className="explorar-container">
+        <button className="btn" onClick={() => history.push('/explorar/comidas')}>
           Explorar Comidas
         </button>
-        <button onClick={() => history.push('/explorar/bebidas')}>
-          Explorar Comidas
+        <button className="btn" onClick={() => history.push('/explorar/bebidas')}>
+          Explorar Bebidas
         </button>
       </div>
     </div>
