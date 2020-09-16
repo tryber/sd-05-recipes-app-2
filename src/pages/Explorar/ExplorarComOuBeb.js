1cf9eccb584b5eed595fad064d17e540c0c83c61
RenataNovais <rnovaiscs@gmail.com>
2020-09-15 22:41:52 -0300

    função random criada. pagina explorar comidas ou bebidas ok

--- src/pages/Explorar/ExplorarComOuBeb.js
+++ src/pages/Explorar/ExplorarComOuBeb.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import AppContext from '../../contexts/AppContext';
+import * as api from '../../services/api';
 
 const typeOfExplore = [
   {datatId: 'explore-by-ingredient', name: 'ingredientes', text: 'Por Ingredientes'},
@@ -9,8 +11,24 @@ const typeOfExplore = [
   {datatId: 'explore-surprise', name: 'surprise', text: 'Me Surpreenda!'},
 ]
 
+const getRandom = (local, history, setDetails) => {
+  if (local === 'comidas') {
+    api.mealRandom().then(data => {
+      setDetails(data.meals[0])
+      history.push(`/comidas/${data.meals[0].idMeal}`)
+    });
+  }
+  if (local === 'bebidas') {
+    api.drinkRandom().then(data => {
+      setDetails(data.drinks[0])
+      history.push(`/bebidas/${data.drinks[0].idDrink}`)
+    });
+  }
+}
+
 export default function ExplorarComOuBeb() {
   const history = useHistory();
+  const { setDetails } = useContext(AppContext);
   const [local, setLocal] = useState();
   const [btns, setBtns] = useState(typeOfExplore);
   useEffect(() => {
@@ -29,6 +47,8 @@ export default function ExplorarComOuBeb() {
     } else if (name === 'area') {
       console.log('yes to no else', name);
       history.push(`/explorar/comidas/${name}`);
+    } else if (name === 'surprise') {
+      getRandom(local, history, setDetails);
     }
   }
 
