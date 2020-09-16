96c916dc46e8723cda09c2015c2b58166e2a8583
rfreitasbatista <rfreitasbatista@gmail.com>
2020-09-16 18:59:47 -0300

    Card Horintal criado

--- src/pages/Perfil/ReceitasFavoritas/ReceitasFavoritas.js
+++ src/pages/Perfil/ReceitasFavoritas/ReceitasFavoritas.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../../components/Header';
+import HCard from '../../../components/HorizontalCard';
 
-export default function ReceitasFavoritas() {
+function ReceitasFeitas() {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const favRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (favRecipes) {
+      setFavorites(favRecipes);
+    }
+  }, []);
+  console.log(favorites);
   return (
-    <Header />
+    <div>
+      <Header />
+      <div>
+        <button data-testid="filter-by-all-btn">Todas</button>
+        <button data-testid="filter-by-food-btn">Comidas</button>
+        <button data-testid="filter-by-drink-btn">Bebidas</button>
+      </div>
+      {favorites.map((card, index) => (
+        <HCard card={card} index={index} />
+      ))}
+    </div>
   );
 }
+
+export default ReceitasFeitas;
