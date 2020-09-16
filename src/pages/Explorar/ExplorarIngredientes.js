3db917000bf3e26874bd531553061f1b219f7dcf
RenataNovais <rnovaiscs@gmail.com>
2020-09-16 01:06:28 -0300

    requisitos da pag de explorar ingredientes ok

--- src/pages/Explorar/ExplorarIngredientes.js
+++ src/pages/Explorar/ExplorarIngredientes.js
@@ -1,4 +1,3 @@
-import { lowerCase, upperCase } from 'lodash';
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../../components/Footer';
@@ -7,14 +6,15 @@ import Loading from '../../components/Loading';
 import AppContext from '../../contexts/AppContext';
 import * as api from '../../services/api';
 
+const urlMeal = (item) =>
+  `https://www.themealdb.com/images/ingredients/${item.strIngredient}-Small.png`;
+const urlDrink = (item) =>
+  `https://www.thecocktaildb.com/images/ingredients/${item.strIngredient1}-Small.png`;
+
 export default function ExplorarIngredientes() {
   const history = useHistory();
-  const { filteredData, setFilteredData, Meal, setMeal } = useContext(AppContext);
+  const { setFilteredData, Meal, setMeal } = useContext(AppContext);
   const [list, setList] = useState([]);
-  const urlMeal = (item) =>
-    `https://www.themealdb.com/images/ingredients/${item.strIngredient}-Small.png`;
-  const urlDrink = (item) =>
-    `https://www.thecocktaildb.com/images/ingredients/${lowerCase(item.strIngredient1)}-Small.png`;
 
   useEffect(() => {
     if (history.location.pathname === '/explorar/comidas/ingredientes') {
@@ -25,16 +25,27 @@ export default function ExplorarIngredientes() {
       api.drinkListIng().then((data) => setList(data.drinks));
       setMeal(false);
     }
-  }, [])
+  }, []);
 
-  if (list.length === 0) return <Loading />
+  const clickIng = (ing) => {
+    if (Meal) {
+      api.byMealIngredient(ing.strIngredient).then((data) => setFilteredData(data.meals));
+      history.push('/comidas');
+    } else {
+      api.byDrinkIngredient(ing.strIngredient1).then((data) => setFilteredData(data.drinks));
+      history.push('/bebidas');
+    }
+  };
+
+  if (list.length === 0) return <Loading />;
   return (
     <div>
       <Header />
       {list.filter((ing, i) => i < 12).map((item, i) =>
         <button
+          onClick={() => clickIng(item)}
           data-testid={`${i}-ingredient-card`}
-          className='card-rec'
+          className="card-rec"
         >
           <div className="card">
             <img
@@ -44,15 +55,12 @@ export default function ExplorarIngredientes() {
               data-testid={`${i}-card-img`}
             />
             <div className="card-body card-description">
-              <p
-                className="card-title d-flex flex-column justify-content-end align-items-center"
-                data-testid={`${i}-card-name`}
-              >
+              <p data-testid={`${i}-card-name`}>
                 {Meal ? item.strIngredient : item.strIngredient1}
               </p>
             </div>
           </div>
-        </button>
+        </button>,
       )}
       <Footer />
     </div>
