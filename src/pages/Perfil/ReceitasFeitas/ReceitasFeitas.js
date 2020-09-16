9c7d33d23aa2e58518453d824866d8d1458ac282
marinabrcls <barcelos.marina@gmail.com>
2020-09-16 18:58:10 -0300

    filters

--- src/pages/Perfil/ReceitasFeitas/ReceitasFeitas.js
+++ src/pages/Perfil/ReceitasFeitas/ReceitasFeitas.js
@@ -5,31 +5,44 @@ import Header from '../../../components/Header';
 // falta: e se não tiver nada no localstorage?
 const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
 
-// mepear as receitas feitas em cards (usar componente card??)
-function mapDoneRecipes() {
-  doneRecipes.map(() => ())
-}
-
+// mepear cards (card comum para favs/feitas)
+// clicar no card leva pro detalhes
 // comida: card deve ter 2 tags de categoria
 // bebida: card deve dizer se é alcoolica ou não
 // botão de compartilhar copia url
-// filtros: food, drinks, all
-// clicar no card leva pro detalhes
-function ReceitasFeitas() {
+function mapDoneRecipes(filteredRecipes) {
+  filteredRecipes.map(() => ())
+}
+
+
+export default function ReceitasFeitas() {
+  const [filters, setFilters] = useState(doneRecipes);
   return (
     <div>
       <Header />
       <div>
-        <button data-testid="filter-by-all-btn">Todas</button>
-        <button data-testid="filter-by-food-btn">Comidas</button>
-        <button data-testid="filter-by-drink-btn">Bebidas</button>
+        <button
+          data-testid="filter-by-all-btn"
+          onClick={() => setFilters(doneRecipes)}
+        >
+          Todas
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          onClick={() => setFilters(doneRecipes.filter((recipe) => recipe.type === 'comida'))}
+        >
+          Comidas
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          onClick={() => setFilters(doneRecipes.filter((recipe) => recipe.type === 'bebida'))}
+        >
+          Bebidas
+        </button>
       </div>
       <div>
-        {mapDoneRecipes()}
+        {mapDoneRecipes(filters)}
       </div>
     </div>
-
   );
 }
-
-export default ReceitasFeitas;
