f72e0a385cf47592656256037a6f0817ea37103a
marinabrcls <barcelos.marina@gmail.com>
2020-09-16 20:14:26 -0300

    codeclimate after merge

--- src/pages/Perfil/ReceitasFeitas/ReceitasFeitas.js
+++ src/pages/Perfil/ReceitasFeitas/ReceitasFeitas.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import Header from '../../../components/Header';
 import HCard from '../../../components/HorizontalCard';
 
