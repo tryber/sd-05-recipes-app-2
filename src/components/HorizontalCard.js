55595b65f0fb4e4d569b832589b489e446eb6665
rfreitasbatista <rfreitasbatista@gmail.com>
2020-09-16 19:46:34 -0300

    tela de favoritos ok

--- src/components/HorizontalCard.js
+++ src/components/HorizontalCard.js
@@ -1,17 +1,69 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import shareI from '../images/shareIcon.svg';
 import blackHI from '../images/blackHeartIcon.svg';
+import { useHistory } from 'react-router-dom';
+import AppContext from '../contexts/AppContext';
+import * as storage from '../services/localStorage';
+
+function handleClick(history, type, id) {
+  if (type === 'comida') {
+    history.push(`/comidas/${id}`);
+  } else {
+    history.push(`/bebidas/${id}`);
+  }
+}
+
+function shareBt(id, type, setCopied) {
+  let textCopy;
+  let linkToCopy;
+  if (type === 'comida') {
+    linkToCopy = `http://localhost:3000/comidas/${id}`;
+  } else {
+    linkToCopy = `http://localhost:3000/bebidas/${id}`;
+  }
+  textCopy = document.createElement('textarea');
+  textCopy.innerText = linkToCopy;
+  document.body.appendChild(textCopy);
+  textCopy.select();
+  document.execCommand('copy');
+  textCopy.remove();
+
+  setCopied(true);
+  setTimeout(() => {
+    setCopied(false);
+  }, 5000);
+}
+
+function disFav(id, type, setFav) {
+  const histo = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  let removing;
+  if (type === 'comida') {
+    removing = histo.filter((each) => each.id !== id);
+  } else {
+    removing = histo.filter((each) => each.id !== id);
+  }
+  localStorage.setItem('favoriteRecipes', JSON.stringify(removing));
+  setFav(true);
+  setTimeout(() => {
+    setFav(false);
+  }, 5000);
+}
 
 function HCard({ card, index }) {
+  const { copied, setCopied, setFav } = useContext(AppContext);
+  const history = useHistory();
+
   return (
     <button className="card-rec">
       <div className="card">
-        <img
-          src={card.image}
-          alt={card.name}
-          className="card-image-top"
-          data-testid={`${index}-horizontal-image`}
-        />
+        <button onClick={() => handleClick(history, card.type, card.id)}>
+          <img
+            src={card.image}
+            alt={card.name}
+            className="card-image-top"
+            data-testid={`${index}-horizontal-image`}
+          />
+        </button>
         <div className="card-body card-description">
           <p
             className="card-title d-flex flex-column justify-content-end align-items-center"
@@ -19,17 +71,20 @@ function HCard({ card, index }) {
           >
             {`${card.type === 'bebida' ? card.alcoholicOrNot : card.area} - ${card.category}`}
           </p>
-          <p data-testid={`${index}-horizontal-name`}>{card.name}</p>
+          <button onClick={() => handleClick(history, card.type, card.id)}>
+            <p data-testid={`${index}-horizontal-name`}>{card.name}</p>
+          </button>
           <div>
-            <button className="det-btn">
+            <button className="det-btn" onClick={() => disFav(card.id, card.type, setFav)}>
               <img
                 src={blackHI}
                 alt="favorite button"
                 data-testid={`${index}-horizontal-favorite-btn`}
               />
             </button>
-            <button className="det-btn">
+            <button className="det-btn" onClick={() => shareBt(card.id, card.type, setCopied)}>
               <img data-testid={`${index}-horizontal-share-btn`} alt="share button" src={shareI} />
+              {copied && <span>Link copiado!</span>}
             </button>
           </div>
         </div>
--- src/pages/Perfil/ReceitasFavoritas/ReceitasFavoritas.js
+++ src/pages/Perfil/ReceitasFavoritas/ReceitasFavoritas.js
@@ -1,28 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../../../components/Header';
 import HCard from '../../../components/HorizontalCard';
+import AppContext from '../../../contexts/AppContext';
 
 function ReceitasFeitas() {
+  const { fav } = useContext(AppContext);
   const [favorites, setFavorites] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
     const favRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
     if (favRecipes) {
       setFavorites(favRecipes);
+      setFilters(favRecipes);
     }
-  }, []);
+  }, [fav]);
   console.log(favorites);
   return (
     <div>
       <Header />
       <div>
-        <button data-testid="filter-by-all-btn">Todas</button>
-        <button data-testid="filter-by-food-btn">Comidas</button>
-        <button data-testid="filter-by-drink-btn">Bebidas</button>
+        <button data-testid="filter-by-all-btn" onClick={() => setFilters(favorites)}>
+          Todas
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          onClick={() => setFilters(favorites.filter((recipe) => recipe.type === 'comida'))}
+        >
+          Comidas
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          onClick={() => setFilters(favorites.filter((recipe) => recipe.type === 'bebida'))}
+        >
+          Bebidas
+        </button>
       </div>
-      {favorites.map((card, index) => (
+      {filters.map((card, index) => (
         <HCard card={card} index={index} />
       ))}
+      {fav && <span>Favoritos atualizados</span>}
     </div>
   );
 }
