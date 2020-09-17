4794e0514320c8c6428fbdc3b37d1f5c2cdd08af
RenataNovais <rnovaiscs@gmail.com>
2020-09-17 19:59:18 -0300

    testes search-bar bebidas ok - falta alert

--- src/tests/search_bar.test.js
+++ src/tests/search_bar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import Provider from '../contexts/Provider';
 import Header from '../components/Header';
@@ -12,6 +12,10 @@ jest.spyOn(api, 'mealCategories').mockImplementation(() => mockAPI.mockSuccessCa
 jest.spyOn(api, 'drinkCategories').mockImplementation(() => mockAPI.mockSuccessCatDrink);
 jest.spyOn(api, 'byMealIngredient').mockImplementation(() => mockAPI.mockIngredChicken);
 jest.spyOn(api, 'byMealName').mockImplementation(() => mockAPI.mockNameSoup);
+jest.spyOn(api, 'byMealFirstLetter').mockImplementation(() => mockAPI.mockA);
+jest.spyOn(api, 'byDrinkIngredient').mockImplementation(() => mockAPI.mockIngredLemon);
+jest.spyOn(api, 'byDrinkName').mockImplementation(() => mockAPI.mockNameGin);
+jest.spyOn(api, 'byDrinkFirstLetter').mockImplementation(() => mockAPI.mockADrink);
 
 describe('Deve ter uma seção de botões para filtrar por categorias', () => {
   it('Tela de comida deve ter categorias e data-test-ids corretos', async () => {
@@ -83,9 +87,9 @@ describe('Todos os elementos devem respeitar os atributos descritos no protótip
   });
 });
 
-describe('A barra de busca deve mudar a forma como serão filtradas as receitas', () => {
+describe('A barra de busca deve mudar a forma como serão filtradas as receitas de comida', () => {
   it('Se o radio selecionado for Ingrediente, a busca na API é feita corretamente pelo ingrediente', async () => {
-    const { history, getByTestId } = renderWithRouter(
+    const { getByTestId } = renderWithRouter(
       <Provider>
         <Home />
       </Provider>,
@@ -102,17 +106,153 @@ describe('A barra de busca deve mudar a forma como serão filtradas as receitas'
     const searchBtn = getByTestId('exec-search-btn');
     fireEvent.click(searchBtn);
     await waitFor(() => expect(api.byMealIngredient).toHaveBeenCalled());
+  })
 
+  it('Se o radio selecionado for Nome, a busca na API é feita corretamente pelo nome', async () => {
+    const { getByTestId } = renderWithRouter(
+      <Provider>
+        <Home />
+      </Provider>,
+      { route: '/comidas' }
+    );
+
+    await waitFor(() => expect(api.defaultMeals).toHaveBeenCalled());
+    const searchIcon = getByTestId('search-top-btn');
+    fireEvent.click(searchIcon);
+
+    const inputSearch = getByTestId('search-input');
     fireEvent.change(inputSearch, { target: { value: 'soup' }});
     const nameRadio = getByTestId('name-search-radio');
     fireEvent.click(nameRadio);
+    const searchBtn = getByTestId('exec-search-btn');
     fireEvent.click(searchBtn);
     await waitFor(() => expect(api.byMealName).toHaveBeenCalled());
+  });
+  it('Se o radio selecionado for Primeira Letra, a busca na API é feita corretamente pela primeira letra', async () => {
+    const { getByTestId } = renderWithRouter(
+      <Provider>
+        <Home />
+      </Provider>,
+      { route: '/comidas' }
+    );
 
+    await waitFor(() => expect(api.defaultMeals).toHaveBeenCalled());
+    const searchIcon = getByTestId('search-top-btn');
+    fireEvent.click(searchIcon);
+
+    const inputSearch = getByTestId('search-input');
     fireEvent.change(inputSearch, { target: { value: 'a' }});
     const firstLetRadio = getByTestId('first-letter-search-radio');
-    fireEvent.click(firstLetRadioRadio);
+    fireEvent.click(firstLetRadio);
+    const searchBtn = getByTestId('exec-search-btn');
     fireEvent.click(searchBtn);
     await waitFor(() => expect(api.byMealFirstLetter).toHaveBeenCalled());
+  });
+  // it('Se o radio selecionado for Primeira letra e a busca na API for feita com mais de uma letra, deve-se exibir um alert', async () => {
+  //   const { getByTestId, getByRole } = renderWithRouter(
+  //     <Provider>
+  //       <Home />
+  //     </Provider>,
+  //     { route: '/comidas' }
+  //   );
+
+  //   await waitFor(() => expect(api.defaultMeals).toHaveBeenCalled());
+  //   const searchIcon = getByTestId('search-top-btn');
+  //   fireEvent.click(searchIcon);
+
+  //   const inputSearch = getByTestId('search-input');
+  //   fireEvent.change(inputSearch, { target: { value: 'aaa' }});
+  //   const firstLetRadio = getByTestId('first-letter-search-radio');
+  //   fireEvent.click(firstLetRadio);
+  //   const searchBtn = getByTestId('exec-search-btn');
+  //   fireEvent.click(searchBtn);
+  //   const alert = getByRole('alert');
+  //   console.log(alert);
+  //   expect(getByRole('alert')).toBeInTheDocument();
+  // });
+})
+
+describe('A barra de busca deve mudar a forma como serão filtradas as receitas de bebida', () => {
+  it('Se o radio selecionado for Ingrediente, a busca na API é feita corretamente pelo ingrediente', async () => {
+    const { getByTestId } = renderWithRouter(
+      <Provider>
+        <Home />
+      </Provider>,
+      { route: '/bebidas' }
+    );
+    await waitFor(() => expect(api.defaultDrinks).toHaveBeenCalled());
+    const searchIcon = getByTestId('search-top-btn');
+    fireEvent.click(searchIcon);
+
+    const inputSearch = getByTestId('search-input');
+    fireEvent.change(inputSearch, { target: { value: 'lemon' }});
+    const ingredRadio = getByTestId('ingredient-search-radio');
+    fireEvent.click(ingredRadio);
+    const searchBtn = getByTestId('exec-search-btn');
+    fireEvent.click(searchBtn);
+    await waitFor(() => expect(api.byDrinkIngredient).toHaveBeenCalled());
   })
+
+  it('Se o radio selecionado for Nome, a busca na API é feita corretamente pelo nome', async () => {
+    const { getByTestId } = renderWithRouter(
+      <Provider>
+        <Home />
+      </Provider>,
+      { route: '/bebidas' }
+    );
+
+    await waitFor(() => expect(api.defaultDrinks).toHaveBeenCalled());
+    const searchIcon = getByTestId('search-top-btn');
+    fireEvent.click(searchIcon);
+
+    const inputSearch = getByTestId('search-input');
+    fireEvent.change(inputSearch, { target: { value: 'gin' }});
+    const nameRadio = getByTestId('name-search-radio');
+    fireEvent.click(nameRadio);
+    const searchBtn = getByTestId('exec-search-btn');
+    fireEvent.click(searchBtn);
+    await waitFor(() => expect(api.byDrinkName).toHaveBeenCalled());
+  });
+  it('Se o radio selecionado for Primeira Letra, a busca na API é feita corretamente pela primeira letra', async () => {
+    const { getByTestId } = renderWithRouter(
+      <Provider>
+        <Home />
+      </Provider>,
+      { route: '/bebidas' }
+    );
+
+    await waitFor(() => expect(api.defaultDrinks).toHaveBeenCalled());
+    const searchIcon = getByTestId('search-top-btn');
+    fireEvent.click(searchIcon);
+
+    const inputSearch = getByTestId('search-input');
+    fireEvent.change(inputSearch, { target: { value: 'a' }});
+    const firstLetRadio = getByTestId('first-letter-search-radio');
+    fireEvent.click(firstLetRadio);
+    const searchBtn = getByTestId('exec-search-btn');
+    fireEvent.click(searchBtn);
+    await waitFor(() => expect(api.byDrinkFirstLetter).toHaveBeenCalled());
+  });
+  // it('Se o radio selecionado for Primeira letra e a busca na API for feita com mais de uma letra, deve-se exibir um alert', async () => {
+  //   const { getByTestId, getByRole } = renderWithRouter(
+  //     <Provider>
+  //       <Home />
+  //     </Provider>,
+  //     { route: '/comidas' }
+  //   );
+
+  //   await waitFor(() => expect(api.defaultMeals).toHaveBeenCalled());
+  //   const searchIcon = getByTestId('search-top-btn');
+  //   fireEvent.click(searchIcon);
+
+  //   const inputSearch = getByTestId('search-input');
+  //   fireEvent.change(inputSearch, { target: { value: 'aaa' }});
+  //   const firstLetRadio = getByTestId('first-letter-search-radio');
+  //   fireEvent.click(firstLetRadio);
+  //   const searchBtn = getByTestId('exec-search-btn');
+  //   fireEvent.click(searchBtn);
+  //   const alert = getByRole('alert');
+  //   console.log(alert);
+  //   expect(getByRole('alert')).toBeInTheDocument();
+  // });
 })
