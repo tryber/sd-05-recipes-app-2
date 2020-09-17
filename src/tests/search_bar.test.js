fd5a0920cac9f1327d8c2bce70b295891b3f5361
RenataNovais <rnovaiscs@gmail.com>
2020-09-17 15:55:08 -0300

    teste search_bar

---
+++ src/tests/search_bar.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import Provider from '../contexts/Provider';
+import Header from '../components/Header';
+import App from '../App';
+import Home from '../pages/Home/Home';
