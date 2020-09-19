457861ab7f5d5bfd72b294f8b204f7af75da6b9c
RenataNovais <rnovaiscs@gmail.com>
2020-09-19 17:29:53 -0300

    explorar por area 100%

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,6 @@ import { useHistory } from 'react-router-dom';
 import './searchbar.css';
 import * as api from '../services/api';
 import AppContext from '../contexts/AppContext';
-// comentário pra forçar reavaliar
 
 function filterAPIComidas(ing, type, setResults) {
   switch (type) {
